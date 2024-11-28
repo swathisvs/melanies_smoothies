583f31beeca9bf057ccf0bc9199a96f8f72fd999
swathisvs <swathi.sv@genpact.com>
2024-11-28 10:06:38 +0530

    Update Streamlit_app.py
    Added submit button

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -46,4 +46,15 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    
+    #st.write(my_insert_stmt)
+    #st.stop
+    
+    time_to_insert = st.button ('Submit Order')
 
+    if time_to_insert:
+   
+     session.sql(my_insert_stmt).collect()
+     st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
